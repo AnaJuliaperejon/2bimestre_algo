9ca7ca805bc69b63819fc107caa353bbbddab669
Ana Julia Perejon <anajp1008@gmail.com>
2025-05-26 15:24:28 -0400

    Add files via upload

--- testando_definicoes.py
+++ testando_definicoes.py
@@ -1,21 +1,46 @@
-compras = ["pao", "cafe","leite"]
-print(compras)
+# compras = ["pao", "cafe","leite"]
+# print(compras)
 
-#pode ser removido pelo nome do produto ou pelo indice.
-#compras.remove("cafe").
-compras.remove(compras[0])
-print(compras)
+# #pode ser removido pelo nome do produto ou pelo indice.
+# #compras.remove("cafe").
+# compras.remove(compras[0])
+# print(compras)
 
-#append adiciona um item de cada vez, ao final da lista.
-compras.append("açucar")
-print(compras)
+# #append adiciona um item de cada vez, ao final da lista.
+# compras.append("açucar")
+# print(compras)
 
-#é preciso criar uma nova lista para receber os elementos ordenados da lista antiga
-compras_ordenada = sorted(compras)
-print(compras_ordenada)
+# #é preciso criar uma nova lista para receber os elementos ordenados da lista antiga
+# compras_ordenada = sorted(compras)
+# print(compras_ordenada)
 
-#tanto faz o nome em que esta após o for, nesse caso "panela" é o identificador  dos itens de dentro da lista.
-for panela in compras:
-    print("-", panela)
+# #tanto faz o nome em que esta após o for, nesse caso "panela" é o identificador  dos itens de dentro da lista.
+# for panela in compras:
+#     print("-", panela)
 
+
+#listas []
+#dicionarios {chave:valor}
+#tuplas = ()
+#conjuntos = set()
+
+#lista
+# teste=[1,2,3, 5]
+# print (teste)
+# teste.append(4)
+# print (teste)
+
+#tupla nao usa "remove" e "append"
+# teste=(1,2,3, 5)
+# print (teste)
+# teste.remove(1)
+# print (teste)
+
+#conjunto usa "remove" e nao repete item
+teste={1,2,3,5,5}
+print (teste)
+# teste.remove(1)
+# print (teste)
+teste.append(4)
+print (teste)
 
